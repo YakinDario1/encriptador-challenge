cb3f771597983abf600f63f554371b5cf86d4abd
Yakin Dario <dario.mendez@alumnos.udg.mx>
2022-05-08 15:30:52 -0500

    add: boton de copiar

--- js/app.js
+++ js/app.js
@@ -12,8 +12,22 @@ function encriptar() {
     document.getElementById("imgDer").style.display = "none";
     document.getElementById("texto").style.display = "none";
 
-    const resultado = `<p>${txtcifrado}</p>`;
+    const resultado = `<p id="cifrado">${txtcifrado}</p>`;
 
     document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("copiar").style.display = "inherit";
 
 }
+function copiar(id_elemento) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    document.getElementById("botonCopiar").style.border = "5px solid #038C3E";
+    document.getElementById('botonCopiar').innerText = "Copiado";
+    document.getElementById("botonCopiar").style.color = "#038C3E";
+  }
+
+
